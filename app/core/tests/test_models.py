f3106b6801abcf64f0f8932314e4f4586b3752bd
2006GTO <23041534+2006GTO@users.noreply.github.com>
2020-12-14 18:25:11 -0600

    setup tdd, run first migration

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    
+    def test_create_user_with_email_successfull(self):
+        # test creating a new user with an email is successfull
+        email = "test@test.com"
+        password = "guest"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password, password)
+        
+    def test_new_user_email_normalized(self):
+        email = "test@TE.com"
+        user = get_user_model().objects.create_user(email, 'ew')
+        
+        self.assertEqual(user.email, email.lower())
